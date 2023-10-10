ad30cd1c89c31761808ca011d212f937be8fdb60
ClNoah09 <noah.cleeremans@ifosup.wavre.be>
2023-10-10 12:34:55 +0200

    wip

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,53 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <div class="jumbotron">
+    <header>
+      <!-- Background image -->
+      <div class="p-5 text-center bg-image" style="">
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
+          <div class="d-flex justify-content-center align-items-center h-100">
+            <div class="text-white">
+              <h1 class="display-4">Bienvenue sur Mon Portfolio mon Reuf</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Background image -->
+    </header>
     <div class="container">
-      <h1 class="display-4">Bienvenue sur Mon Portfolio mon Reuf </h1>
       <p class="lead">
-        Ici, vous découvrirez mon monde artistique, un lieu où la créativité s'épanouit. À travers mes projets, je vous
-        invite à explorer ma passion pour l'art, la photographie, le design et bien plus encore. Chaque œuvre raconte une
-        histoire, chaque pixel est imprégné de mon engagement envers l'expression artistique. Je vous invite à parcourir
-        ces pages, à vous laisser inspirer, et à partager ce voyage avec moi.
-
-        Bienvenue à bord !
+        Ici, vous découvrirez mon monde artistique, un lieu où la créativité
+        s'épanouit. À travers mes projets, je vous invite à explorer ma passion
+        pour l'art, la photographie, le design et bien plus encore. Chaque œuvre
+        raconte une histoire, chaque pixel est imprégné de mon engagement envers
+        l'expression artistique. Je vous invite à parcourir ces pages, à vous
+        laisser inspirer, et à partager ce voyage avec moi. Bienvenue à bord !
       </p>
       <h2>Projets</h2>
-      <img src="../../public/img/examen-logo.svg" class="rounded float-start" alt="Logo1" width="200" height="200">
-      <img src="../../public/img/Logo.svg" class="rounded float-end" alt="Logo2" width="200" height="200">
-      <RouterLink to="/projets" class="btn btn-primary btn-lg">Voir tous les projets</RouterLink>
+      <img
+        src="../../public/img/examen-logo.svg"
+        class="rounded float-start"
+        alt="Logo1"
+        width="200"
+        height="200"
+      />
+      <img
+        src="../../public/img/Logo.svg"
+        class="rounded float-end"
+        alt="Logo2"
+        width="200"
+        height="200"
+      />
+
+      <RouterLink
+        to="/projets"
+        class="btn btn-primary btn-lg mt-3 d-block mx-auto"
+      >
+        Voir tous les projets
+      </RouterLink>
     </div>
   </div>
 </template>
--- src/pages/Projets.vue
+++ src/pages/Projets.vue
@@ -1,24 +1,48 @@
 <template>
   <h2>Mes Projets</h2>
   <div id="carouselExampleFade" class="carousel slide carousel-fade">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../../public/img/Logo.svg" class="d-block w-100" alt="...">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <a href="#section-specifique">
+          <img
+            src="../../public/img/Logo.svg"
+            class="d-block w-100"
+            alt="..."
+          />
+        </a>
+      </div>
+      <div class="carousel-item" href="#logo1">
+        <img
+          src="../../public/img/examen-logo.svg"
+          class="d-block w-100"
+          alt="..."
+        />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="../../public/img/slider01.jpg"
+          class="d-block w-100"
+          alt="..."
+        />
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="../../public/img/examen-logo.svg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../../public/img/slider01.jpg" class="d-block w-100" alt="...">
-    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleFade"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleFade"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 </template>
